166d2771dac8a7dd3b79d5426f3215ac186a274d
Akshara S <sarunsaji31@gmail.com>
2024-08-19 01:18:38 +0400

    Update wsgi.py

--- staff_transport/wsgi.py
+++ staff_transport/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for the 'django' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "staff_transport.settings")
 
+# Get the WSGI application for use by the web server to forward requests to Django.
 application = get_wsgi_application()
