3c3ddf036d59cb4862566c40b712868e73c18314
Akshara S <sarunsaji31@gmail.com>
2024-08-23 01:56:21 +0400

    Update urls.py

--- staff_transport/urls.py
+++ staff_transport/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 def test_view(request):
     return HttpResponse("The application is running.")
@@ -9,4 +11,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', test_view),  # This is a simple test view to check if the app is running
     path('duty/', include('duty.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
