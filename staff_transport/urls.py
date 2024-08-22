fa81be16041541756f58899b2663fd99412ec785
Akshara S <sarunsaji31@gmail.com>
2024-08-23 01:49:00 +0400

    Update urls.py

--- staff_transport/urls.py
+++ staff_transport/urls.py
@@ -1,24 +1,12 @@
-"""
-URL configuration for staff_transport project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def test_view(request):
+    return HttpResponse("The application is running.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('duty.urls')),
+    path('', test_view),  # This is a simple test view to check if the app is running
+    path('duty/', include('duty.urls')),
 ]
-    
